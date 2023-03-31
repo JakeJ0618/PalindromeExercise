acba3104440887492cff8d534df2aa778d6cbf1e
Jake Johnston <jakejohnston618@gmail.com>
2023-03-31 11:41:51 -0500

    made the palindromes

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("palindrome", false)]
+        [InlineData("racecar", true)]
+        [InlineData("hello",  false)]
+        [InlineData("kayak", true)]
+        [InlineData("Jake", false)]
+        [InlineData("madam", true)]
+        [InlineData("tacocat", true)]
+        public void PalindromeTester(string word, bool expected)
         {
+            //Arrange
+            WordSmith tester = new WordSmith();
 
+            //Act
+            var actual = tester.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
